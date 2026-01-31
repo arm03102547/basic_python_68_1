b4a46a66c6f2f553ddda78f14e7e61d31a444a53
arm03102547 <arm03102547@gmail.com>
2026-01-31 14:11:39 +0700

    Collection of python

---
+++ python3.py
@@ -0,0 +1,59 @@
+"""
+#
+# Part: Python Operators
+# Lists, tuples, Sets, Dictionaries
+#
+"""
+
+"""
+#
+# Lists
+#
+"""
+
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "mellon", "mango"]
+print(this_list)
+print(this_list[1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+# Tuples
+#
+"""
+
+this_tuple = ("apple", "banana", "cherry", "orange", "kiwi", "mellon", "mango")
+print(this_tuple)
+print(this_tuple[1])
+print(type(this_tuple))
+print(len(this_tuple))
+
+
+"""
+#
+# Sets
+#
+"""
+
+this_set = {"apple", "banana", "cherry", "orange", "kiwi", "mellon", "mango"}
+print(this_set)
+print(type(this_set))
+print(len(this_set))
+
+"""
+#
+# Dictionaries
+#
+"""
+
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(this_dict)
+print(this_dict["model"])
+print(type(this_dict))
+print(len(this_dict))
