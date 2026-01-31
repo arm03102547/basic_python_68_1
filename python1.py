c5c5e9fbb3ffa45c3edadfaa47fabf9711d5b156
arm03102547 <arm03102547@gmail.com>
2026-01-31 13:18:10 +0700

    Python Variables

---
+++ python1.py
@@ -0,0 +1,53 @@
+"""
+#
+# Part:  Python Comment
+#
+"""
+
+"""
+v = s / t
+ความเร็ว = ระยะทาง / เวลา
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s)
+"""
+
+print("Hello, World!")
+
+"""
+#
+# Part:  Python Variables
+#
+"""
+
+x = 10
+y = "10"
+print(x, y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: variables Naming
+#
+"""
+
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400
+MYVAR = 500
+
+# my-var = 600
+# 2myvar = 700
+# my var = 800
+
+# camel Case
+myVariableName = 900
+# Pascal Case
+MyVariableName = 1000
+# Snake Case 
+my_variable_name = 1100
