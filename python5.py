387383523594378e5377c7c5df6f0a730f09a9ec
arm03102547 <arm03102547@gmail.com>
2026-01-31 14:38:24 +0700

    Loop of python

---
+++ python5.py
@@ -0,0 +1,26 @@
+"""
+#
+# Loops
+# While Loop
+# 
+"""
+
+i = 1
+while i < 5:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+    
+"""
+#
+# Loops
+# For Loop
+# 
+"""
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:    
+    print(fruit)
+    if fruit == "banana":
+        break
