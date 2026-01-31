e01333549311ff90e9d5e5c8b46f194d2afbb6e0
arm03102547 <arm03102547@gmail.com>
2026-01-31 14:24:08 +0700

    Conditional statements

---
+++ python4.py
@@ -0,0 +1,31 @@
+"""
+#
+# Conditional statements
+#
+"""
+
+a = 10
+b = 20
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+elif a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b") 
+
+if a < b:
+    print("a is less than b")
+if a > b:
+    print("a is greater than b")
+
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+
+if a < b:
+    print("a is less than b")
+else:
+    print("a is greater than b") 
